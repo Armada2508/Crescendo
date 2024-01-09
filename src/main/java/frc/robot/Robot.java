1b72ba30bc150c4105faad316edf7568fc1c371c
Wispy Sparks <wispysparks@gmail.com>
2024-01-09 15:26:56 -0600

    initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+    
+    private RobotContainer robotContainer;
+    
+    @Override
+    public void robotInit() {
+        robotContainer = new RobotContainer();
+    }
+    
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+     @Override
+    public void autonomousInit() {
+        robotContainer.getAutonomousCommand().schedule();;
+    }
+
+    @Override
+    public void teleopInit() {
+        robotContainer.stopEverything();
+    }
+    
+    @Override
+    public void disabledInit() {
+        robotContainer.stopEverything();
+    }
+    
+    @Override
+    public void disabledPeriodic() {}
+    
+    @Override
+    public void disabledExit() {}
+    
+    @Override
+    public void autonomousPeriodic() {}
+    
+    @Override
+    public void autonomousExit() {}
+    
+    @Override
+    public void teleopPeriodic() {}
+    
+    @Override
+    public void teleopExit() {}
+    
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+    
+    @Override
+    public void testPeriodic() {}
+    
+    @Override
+    public void testExit() {}
+    
+}
