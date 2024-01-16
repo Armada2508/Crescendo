5458afe02fd2d52c8a7b49f77350e599790f2878
Wispy Sparks <wispysparks@gmail.com>
2024-01-16 16:55:11 -0600

    drive reversal button

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -14,13 +16,18 @@ import frc.robot.subsystems.VisionSubsystem;
 public class RobotContainer {
 
     private final SmartJoystick joystick = new SmartJoystick(0);
+    private final SmartJoystick buttonBoard = new SmartJoystick(1);
     private final VisionSubsystem visionSubsystem = new VisionSubsystem();
     private final DriveSubsystem driveSubsystem = new DriveSubsystem(visionSubsystem::getEstimatedGlobalPose);
     private final PivotSubsystem pivotSubsystem = new PivotSubsystem();
 
     public RobotContainer() {
-        joystick.bindButtons(12);
-        driveSubsystem.setDefaultCommand(driveSubsystem.joystickDriveCommand(joystick::getY, joystick::getX, joystick::getZ, () -> joystick.getRawButton(12)));
+        int reverseButton = 0;
+        joystick.bindButtons(12); // Drive Slow
+        buttonBoard.bindButtons(reverseButton); // Drive Reverse
+        driveSubsystem.setDefaultCommand(driveSubsystem.joystickDriveCommand(
+            reverseAxisIf(joystick::getY, reverseButton), reverseAxisIf(joystick::getX, reverseButton), reverseAxisIf(joystick::getY, reverseButton), () -> joystick.getRawButton(12)
+        ));
         configureBindings();
     }
 
@@ -36,6 +43,10 @@ public class RobotContainer {
 
     public Command getAutonomousCommand() {
         return Commands.print("No autonomous command configured");
+    }
+
+    private DoubleSupplier reverseAxisIf(DoubleSupplier axis, int button) {
+        return () -> buttonBoard.getRawButton(button) ? -axis.getAsDouble() : axis.getAsDouble();
     }
 
 }
