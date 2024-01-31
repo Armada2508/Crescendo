36d844ae415e2d8fcc2270c3494feb2b75c21287
Tricks1228 <christopp1228@gmail.com>
2024-01-31 16:23:21 -0600

    updated angles and distance to units library

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
 import static frc.robot.commands.Routines.groundIntake;
 import static frc.robot.commands.Routines.scoreSpeakerVision;
 import static frc.robot.commands.Routines.translationToTrajectoryPose;
@@ -14,12 +16,12 @@ import frc.robot.subsystems.IntakeShooterSubsystem;
 public class Autos {
 
     private Autos() {}
-    //! Have to tune accelerations and velocitys
+    //! Have to tune accelerations and velocitys for everything
     public static Command autoSimple(DriveSubsystem driveSubsystem, ArmSubsystem armSubsystem, IntakeShooterSubsystem intakeShooterSubsystem) {
         return turnToSpeaker(driveSubsystem)
         .andThen(scoreSpeakerVision(driveSubsystem, armSubsystem, intakeShooterSubsystem))
-        .andThen(driveSubsystem.turnCommand(0)) //turn to leave //? use vision for angle?
-        .andThen(driveSubsystem.driveDistanceCommand(0, 0, 0)); //drive out //? use vision for distance?
+        .andThen(driveSubsystem.turnCommand(Degrees.of(180))) //turn to leave //? use vision for angle?
+        .andThen(driveSubsystem.driveDistanceCommand(Meters.of(1.5), 0, 0)); //drive out //? adjust distance to leave at week zero
     }
 
     public static Command autoComplex(DriveSubsystem driveSubsystem, ArmSubsystem armSubsystem, IntakeShooterSubsystem intakeShooterSubsystem) {
@@ -28,7 +30,6 @@ public class Autos {
         .andThen(driveSubsystem.trajectoryToPoseCommand(Field.lowerNotePickupLocation)) //assume we go for medium note
         .andThen(groundIntake(armSubsystem, intakeShooterSubsystem))
         .andThen(driveSubsystem.trajectoryToPoseCommand(translationToTrajectoryPose(Field.speakerPos, 0))) //tune angle
-        .andThen(scoreSpeakerVision(driveSubsystem, armSubsystem, intakeShooterSubsystem))
-        .andThen(driveSubsystem.driveDistanceCommand(0, 0, 0)); //? use vision for distance?
+        .andThen(scoreSpeakerVision(driveSubsystem, armSubsystem, intakeShooterSubsystem));
     }
 }
