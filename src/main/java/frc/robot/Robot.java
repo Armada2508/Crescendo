5ce354d21ad84c5cc9b28cc29fd04b4c168253d6
WispySparks <wispysparks@gmail.com>
2024-02-19 20:36:59 -0600

    j

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.Field;
 import frc.robot.lib.logging.Loggable;
 import frc.robot.lib.logging.NTLogger;
-import frc.robot.lib.music.TalonMusic;
 
 public class Robot extends TimedRobot implements Loggable {
     
@@ -28,7 +27,7 @@ public class Robot extends TimedRobot implements Loggable {
         SmartDashboard.putData(Field.simulatedField);
         NTLogger.register(this);
         robotContainer = new RobotContainer();
-        TalonMusic.playStartupTune();
+        // TalonMusic.playStartupTune();
     }
     
     @Override
