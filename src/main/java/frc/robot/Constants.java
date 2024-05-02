5b5b8e4fce635e7bbe14d2b2c1f13086a0f41c02
Wispy Sparks <wispysparks@gmail.com>
2024-05-02 15:51:34 -0500

    raise angle to not break robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ public class Constants {
         public static final Measure<Angle> climbAngle = Degrees.of(28); // Just hovering off the ground
         public static final Measure<Angle> speakerBaseAngle = Degrees.of(41.5);
         public static final Measure<Angle> ampAngle = Degrees.of(76.0); 
-        public static final Measure<Angle> retractAngle = Degrees.of(40); // Safe to retract pistons
+        public static final Measure<Angle> retractAngle = Degrees.of(45); // Safe to retract pistons
         public static final Measure<Angle> stowAngle = Degrees.of(60);
         public static final Measure<Angle> minAngle = Degrees.of(18);
         public static final Measure<Angle> maxAngle = Degrees.of(80);
